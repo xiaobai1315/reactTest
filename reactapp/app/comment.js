31abe5454cd19e5d8ddcc9501fb30ca7e4ad2a0f
xiaobai1315 <xiaobai1315@yeah.net>
2017-09-12 07:29:23 +0800

    commit

---
+++ reactapp/app/comment.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+class CommentBox extends React.Component{
+    render(){
+        return(
+            <div className="ui comments">
+                <h1>评论</h1>
+                <div className="ui divider"></div>
+            </div>
+        )
+    }
+}
+
+export { CommentBox as default };
---
+++ reactapp/app/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import React from 'react';
+import ReactDom from 'react-dom'
+import CommentBox from './CommentBox';
+
+ReactDom.render(
+    <CommentBox/>,
+    document.getElementById("app")
+)
+
+ReactDom.render();
